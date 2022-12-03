0838cf65b5186745a7a6c917d6c71349daf7ddfc
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-12-03 22:31:25 +0630

    Ep-8-2-way-of-using-images-in-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import QAImage from './assets/image.jpg';
 
 function App() {
 
   let name = "Hlaing Min Than";
 
   return (
-    <h1>Hello {name}</h1>
+    <div>
+      <h1>Hello {name}</h1>
+
+      {/* 1st way -  public folder way */}
+
+      {/* absolute path */}
+      {/* <img src="http://localhost:3000/image.jpg" alt="" /> */}
+      {/* relative path */}
+      {/* <img src="/image.jpg" alt="" /> */}
+
+      {/* 2nd import */}
+      <img src={QAImage} alt="" />
+    </div>
   );
 }
 
