83dfb95e6eee9873a7f1f720913902d9862303f3
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-12-15 03:05:36 +0630

    Ep-13-working-with-list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import { useState } from 'react';
 function App() {
 
   let [name,setName] = useState("Hlaing Min Than");// [getter,setterFun]
+  let [posts,setPosts]=useState([
+    {
+      id : 1,
+      title: 'First post'
+    },
+    {
+      id : 2,
+      title: 'Second post'
+    },
+    {
+      id : 3,
+      title: 'Third post'
+    }
+  ]);
+  console.log(posts);
 
   let changeName = () => {
     setName('Aung Aung');
@@ -14,6 +29,13 @@ function App() {
     <div className='app'>
       <h1>Hello {name}</h1>
       <button onClick={changeName}>change name</button>
+
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
