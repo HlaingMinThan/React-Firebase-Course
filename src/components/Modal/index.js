01400fd5f50203fe4a64165707d988497af768ee
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-09 23:05:30 +0630

    Ep-21-Portal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import './index.css';
 
 export default function Modal({children}) {
   return (
-    <div className="modal-backdrop">
+    ReactDOM.createPortal(
+      <div className="modal-backdrop">
         <div className="modal">
            {children}
         </div>
-    </div>
+    </div>,
+    document.getElementById('modal')
+    )
   )
 }
