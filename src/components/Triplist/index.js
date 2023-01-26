9fa42afab368bd95e4af1479ccb4986ddfd8a8ae
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-26 23:54:01 +0630

    Ep-32-useEffect-dependency-example

--- src/components/Triplist/index.js
+++ src/components/Triplist/index.js
@@ -3,20 +3,24 @@ import React, { useEffect, useState } from 'react'
 export default function Index() {
 
   let [trips, setTrips ] = useState([]);
+  let [url, setUrl] = useState('http://localhost:3001/trips');
 
   useEffect(() => {
-    fetch('http://localhost:3001/trips')
+    fetch(url)
     .then(res => res.json())
     .then(data => {
       setTrips(data);
     })
-  },[]);
+  },[url]);
 
   console.log(trips)
 
   return (
     <div>
       <h1>Ready to go ?</h1>
+
+      <button onClick={()=>setUrl('http://localhost:3001/trips')}>all</button>
+      <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>Trips in Myanmar</button>
 
       <ul>
         {trips.map(trip => (
