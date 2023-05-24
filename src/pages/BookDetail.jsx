9488bed35b1415d96a99ac4c2e53ca76f849b184
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-05-24 20:08:59 +0630

    Ep-60-Book-Detail-Page-Design

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
+import bookImg from '../assets/book.png';
 
 export default function BookDetail() {
     let { id } = useParams();
@@ -11,7 +12,22 @@ export default function BookDetail() {
             {error && <p>{error}</p>}
             {loading && <p>loading ....</p>}
             {book && (
-                <h1>{book.title}</h1>
+                <div className='grid grid-cols-2'>
+                    <div>
+                        <img src={bookImg} alt="" className='w-[80%]' />
+                    </div>
+                    <div className='space-y-4'>
+                        <h1 className='text-3xl font-bold'>{book.title}</h1>
+                        <div className='space-x-3'>
+                            {book.categories.map(cateogry => (
+                                <span className='bg-blue-500 text-white rounded-full text-sm px-2 py-1' key={cateogry}>{cateogry}</span>
+                            ))}
+                        </div>
+                        <p>
+                            {book.description}
+                        </p>
+                    </div>
+                </div>
             )}
         </>
     )
