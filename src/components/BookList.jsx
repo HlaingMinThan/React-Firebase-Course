457ed6bb91485ea5269a23e3600bfbdab40e2833
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-06-12 15:19:52 +0630

    Ep-74-get-firebase-documents-(books)

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import book from '../assets/book.png';
-import useFetch from '../hooks/useFetch';
 import { Link, useLocation } from 'react-router-dom';
 import useTheme from '../hooks/useTheme';
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function BookList() {
 
@@ -10,7 +11,21 @@ export default function BookList() {
     let params = new URLSearchParams(location.search);
     let search = params.get('search')
 
-    let { data: books, loading, error } = useFetch(`http://localhost:3000/books${search ? `?q=${search}` : ''}`);
+    let [error, setError] = useState('');
+    let [books, setBooks] = useState([]);
+    let [loading, setLoading] = useState(false);
+
+    useEffect(function () {
+        let ref = collection(db, 'books');
+        getDocs(ref).then(docs => {
+            let books = [];
+            docs.forEach(doc => {
+                let book = { id: doc.id, ...doc.data() }
+                books.push(book)
+            })
+            setBooks(books)
+        })
+    }, [])
 
     if (error) {
         return <p>{error}</p>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,3 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+let db = getFirestore(app);
+
+export { db }
