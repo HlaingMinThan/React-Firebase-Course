8edcd9ba312da41237b2d58542316b88b4963d64
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-06-20 16:44:52 +0630

    Ep-88-React-Router-Route-Guard-Pattern

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,5 @@
 import {
+    Navigate,
     RouterProvider,
     createBrowserRouter,
 } from "react-router-dom";
@@ -14,6 +15,9 @@ import React, { useContext } from 'react'
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function index() {
+    let { authReady, user } = useContext(AuthContext);
+
+    const isAuthenticated = Boolean(user);
 
     const router = createBrowserRouter([
         {
@@ -22,37 +26,35 @@ export default function index() {
             children: [
                 {
                     path: "",
-                    element: <Home />
+                    element: isAuthenticated ? <Home /> : <Navigate to="/login" />
                 },
                 {
                     path: "/books/:id",
-                    element: <BookDetail />
+                    element: isAuthenticated ? <BookDetail /> : <Navigate to="/login" />
                 },
                 {
                     path: "/create",
-                    element: <BookForm />
+                    element: isAuthenticated ? <BookForm /> : <Navigate to="/login" />
                 },
                 {
                     path: "/edit/:id",
-                    element: <BookForm />
+                    element: isAuthenticated ? <BookForm /> : <Navigate to="/login" />
                 },
                 {
                     path: "/search",
-                    element: <Search />
+                    element: isAuthenticated ? <Search /> : <Navigate to="/login" />
                 },
                 {
                     path: "/register",
-                    element: <Register />
+                    element: !isAuthenticated ? <Register /> : <Navigate to="/" />
                 },
                 {
                     path: "/login",
-                    element: <Login />
+                    element: !isAuthenticated ? <Login /> : <Navigate to="/" />
                 },
             ]
         },
     ]);
-
-    let { authReady } = useContext(AuthContext);
 
     return (
         authReady && <RouterProvider router={router} />
