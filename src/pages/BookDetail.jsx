eb93b31b1e8dda2b6da4775d375ad9b87779c841
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-06-28 14:43:25 +0630

    Ep-93-Note-Feature-Design

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import bookImg from '../assets/book.png';
 import useTheme from '../hooks/useTheme';
 
 import useFirestore from '../hooks/useFirestore';
@@ -19,22 +17,43 @@ export default function BookDetail() {
             {error && <p>{error}</p>}
             {loading && <p>loading ....</p>}
             {book && (
-                <div className={`grid grid-cols-2 h-screen ${isDark ? 'text-white' : ''}`}>
-                    <div>
-                        <img src={bookImg} alt="" className='w-[80%]' />
-                    </div>
-                    <div className='space-y-4'>
-                        <h1 className='text-3xl font-bold'>{book.title}</h1>
-                        <div className='space-x-3'>
-                            {book.categories.map(cateogry => (
-                                <span className='bg-blue-500 text-white rounded-full text-sm px-2 py-1' key={cateogry}>{cateogry}</span>
-                            ))}
+                <>
+                    <div className={`grid grid-cols-2 ${isDark ? 'text-white' : ''}`}>
+                        <div>
+                            <img src={book.cover} alt="" className='w-[80%]' />
                         </div>
-                        <p>
-                            {book.description}
-                        </p>
+                        <div className='space-y-4'>
+                            <h1 className='text-3xl font-bold'>{book.title}</h1>
+                            <div className='space-x-3'>
+                                {book.categories.map(cateogry => (
+                                    <span className='bg-blue-500 text-white rounded-full text-sm px-2 py-1' key={cateogry}>{cateogry}</span>
+                                ))}
+                            </div>
+                            <p>
+                                {book.description}
+                            </p>
+                        </div>
                     </div>
-                </div>
+                    <div>
+                        <h3 className='font-bold text-xl text-primary my-3 text-center'>My Notes</h3>
+                        <textarea className='p-3 shadow-md border-2 bg-gray-50 w-full' name="" id="" cols="30" rows="5"></textarea>
+                        <button className='text-white bg-primary px-3 py-2 rounded-lg my-3 flex items-center gap-1'>
+                            <span className="hidden md:block">Add Note</span>
+                        </button>
+                        <div className='border-2 shadow-md p-3 my-3'>
+                            <div className='flex space-x-3'>
+                                <img src="https://d27v83ov1up738.cloudfront.net/user-profiles/zhW65iWspiv5FRKjp465yKLiQt9kPUhd2L8UDfV0.jpg" alt="" className='w-12 h-12 rounded-full' />
+                                <div>
+                                    <h3>Hlaing min than</h3>
+                                    <div className='text-gray-400'>20.6.2001</div>
+                                </div>
+                            </div>
+                            <div className='mt-3'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste minima, fugit fugiat non, tenetur, culpa cum veritatis animi autem neque temporibus et explicabo iure! Maxime ad tenetur consequatur quasi atque.
+                            </div>
+                        </div>
+                    </div>
+                </>
             )}
         </>
     )
